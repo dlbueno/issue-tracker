ae6a62202d4e21651eb2d575e876e7f2c3c8a1ec
dlbueno <142638660+dlbueno@users.noreply.github.com>
2023-11-24 14:30:08 -0600

    style the active link on the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,27 +1,39 @@
-import { link } from "fs";
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { GiTick } from "react-icons/gi";
+import classnames from "classnames";
 
 const NavBar = () => {
-    const links= [
-        {label: 'Dashboard', href: '/'},
-        {label: 'Issues', href: '/issues'},
-    ]
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/"><GiTick/></Link>
+      <Link href="/">
+        <GiTick />
+      </Link>
       <ul className="flex space-x-6">
-        {links.map(link => 
-            <Link 
-                key={link.href} 
-                className='text-zinc-500 hover:text-zinc-800 transition-colors' 
-                href={link.href}>{link.label}</Link>)}
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hoover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
 };
 
 export default NavBar;
-
-
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Issuepage = () => {
+  return <div>Issue</div>;
+};
+
+export default Issuepage;
